b08e699921c3b793025da203da020376ee8e5b0c
msedou <korda.daaba@gmail.com>
2024-12-19 08:33:02 -0500

    implement add headin functionality

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -2,6 +2,19 @@ import React, { useState } from 'react';
 import './TodoList.css';
 
 const TodoList = () => {
+
+
+    const [todos, setTodos] = useState([]);
+    const [headingInput, setHeadingInput] = useState('');
+    const [listInputs, setListInputs] = useState({});
+
+
+    const headleAddTodo =()=>{
+        if(headingInput.trim() !== ''){
+            setTodos([...todos,{heading: headingInput,lists:[]}]);
+            setHeadingInput('');
+        }
+    };
   
 
   return (
@@ -13,13 +26,21 @@ const TodoList = () => {
             type="text"
             className="heading-input"
             placeholder="Enter heading"
-            
+            value={headingInput}
+            onChange={(e)=>{setHeadingInput(e.target.value);}} // Add onChange event handler to update headingInput state
           />
-          <button className="add-list-button">Add Heading</button>
+          <button className="add-list-button" onClick={handleAddTodo}>Add Heading</button>
         </div>
       </div>
       <div className="todo_main">
-        
+        {todos.map((todo,index)=>(
+            <div key={index} className='todo-card'>
+                <div className='heading_todo'>
+                    <h3>{todo.heading}</h3> {/* Display the heading here */}
+                    <button className='delete-button-heading' onClick={()=> handleDeleteTodo(index)}>Delete Heading </button>
+                </div>
+            </div>
+        ))}
       </div>
     </>
   );
