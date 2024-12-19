d64ccbed423c3cf84861a3fd2d158c32a4df923a
msedou <korda.daaba@gmail.com>
2024-12-19 16:01:32 -0500

    add FetchYogaData component

--- src/Components/FetchYogaData.jsx
+++ src/Components/FetchYogaData.jsx
@@ -1,6 +1,8 @@
-
-
+import React from 'react'
+import UseFetch from './UseFetch'
 const FetchYogaData=()=>{
+    const [data] = UseFetch('https://api.npoint.io/4459a9a10e43812e1152');
+    console.log(data);
     return (
         <>
         </>
