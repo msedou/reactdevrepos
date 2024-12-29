492120f83a2a9878ef4c49fae3053ad1c95ccba5
msedou <korda.daaba@gmail.com>
2024-12-29 14:48:08 -0500

    created add_ons section

--- src/store.js
+++ src/store.js
@@ -1,9 +1,11 @@
 // store.js
 import { configureStore } from '@reduxjs/toolkit';
 import venueReducer from './venueSlice';
+import avReduce from './avSlice';
 
 export default configureStore({
   reducer: {
     venue: venueReducer,
+    av : avReduce,
   },
 });
